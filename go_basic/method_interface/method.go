51d14ebbf58a3c8f9c938007c8660c8202517881
OptimusPrime2001 <trungbts3@gmail.com>
2025-07-30 23:54:34 +0700

    Method more

--- go_basic/method_interface/method.go
+++ go_basic/method_interface/method.go
@@ -16,9 +16,20 @@ func (v Vertext) abs() float64 {
 func (x Liver) jota() int {
 	return int(x) + 2001
 }
+func (u *Vertext) change(f float64) {
+	u.x *= f
+	u.y *= f
+}
+func otherChange(v *Vertext, f float64) {
+	v.x *= f
+	v.y *= f
+}
 func TestMethod() {
-	v := Vertext{3, 4}
+	v := &Vertext{3, 4}
 	trung := Liver(20)
 	fmt.Println(trung.jota())
+	fmt.Println(v.abs())
+	v.change(10)
+	otherChange(v, 10)
 	fmt.Println(v.abs())
 }
